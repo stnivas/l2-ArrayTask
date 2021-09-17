2553aee4ef5c6aebb55c9a787e2cb43ecb547e4b
Nivas <stnivas13@gmail.com>
2021-09-17 17:32:41 +0530

    initial commit

---
+++ src/com/company/RotateArray.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class RotateArray {
+    public void rotate(int[]number){
+        for(int i=0;i<2;i++){
+
+
+        }
+    }
+
+    public static void main(String [] args){
+        RotateArray obj=new RotateArray();
+
+        int[] number={-1,-2,-3,-4,-5};
+        obj.rotate(number);
+    }
+}
