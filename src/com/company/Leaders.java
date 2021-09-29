fa66de1b36ec02c465aabd79a29b27773668b111
Nivas <stnivas13@gmail.com>
2021-09-29 16:53:06 +0530

    commit

--- src/com/company/Leaders.java
+++ src/com/company/Leaders.java
@@ -5,7 +5,7 @@ public class Leaders {
         int count=0;
         for (int i = 0; i <number.length ; i++) {
             for (int j = i + 1; j < number.length; j++) {
-                if (number[i] < number[j]) {
+                if (number[i]<number[j]){
                     count++;
                 }
             }
--- src/com/company/RotateArray.java
+++ src/com/company/RotateArray.java
@@ -3,17 +3,28 @@ package com.company;
 import java.util.ArrayList;
 
 public class RotateArray {
-    public void rotate(int[]number){
-        for(int i=0;i<2;i++){
-
-
+    public ArrayList<Integer> rotate(int[]number,int position) {
+        ArrayList<Integer> list = new ArrayList<>();
+        try {
+            for (int i = position; i < number.length; i++) {
+                list.add(number[i]);
+            }
+            for (int i = 0; i < position; i++) {
+                list.add(number[i]);
+            }
         }
-    }
+          catch (IndexOutOfBoundsException e){
+              System.out.println("please enter the correct input and position");
+          }
+            return list;
+        }
 
     public static void main(String [] args){
         RotateArray obj=new RotateArray();
 
-        int[] number={-1,-2,-3,-4,-5};
-        obj.rotate(number);
+        int[] number={1,2,3,4,5};
+        int position=2;
+       ArrayList<Integer>list= obj.rotate(number,position);
+        System.out.println(list);
     }
 }
